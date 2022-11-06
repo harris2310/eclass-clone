39101e39f474e87ce514e091185897f9c6dde312
harris2310 <harris2310@hotmail.com>
2022-11-06 11:13:29 +0200

    Working on home screen content and ui

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -4,9 +4,9 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div className=" text-center font-semibold text-xl py-4 bg-gray-200">
+    <footer className=" text-center font-semibold text-xl py-4 bg-gray-200">
       @duth
-    </div>
+    </footer>
   );
 };
 
--- components/screens/SignInScreen.tsx
+++ components/screens/SignInScreen.tsx
@@ -1,13 +1,17 @@
-import React, { BaseSyntheticEvent } from "react";
-import { signIn, signOut, getProviders } from "next-auth/react";
+import React from "react";
+import { signIn } from "next-auth/react";
+import { dailyAnnouncements } from "../../utils/constants";
+
+type announcements = {
+  title: string;
+  date: string;
+  announcement: string;
+};
 
 const SignInScreen = ({ providers }: any) => {
-  const handleClick = () => {
-    signIn();
-  };
   return (
     <>
-      <h1 className="my-6 text-xl font-bold text-black">Welcome to E class</h1>
+      <h1 className="my-16 font-bold text-black">Welcome to E class</h1>
       {Object.values(providers).map((provider: any, i) => {
         return (
           <div key={i}>
@@ -20,6 +24,33 @@ const SignInScreen = ({ providers }: any) => {
           </div>
         );
       })}
+      <div className="bg-red-200 text-lg w-6/12 mx-auto my-5 border-orange-400 border-2 p-2">
+        The DUTHNET eClass platform is a complete Electronic Course Management
+        System. Access to the service is done using a simple web browser without
+        the need for external
+      </div>
+      <div className=" w-max mx-auto mt-16">
+        <table className="table-auto w-full border-separate p-2 border-spacing-y-4	border border-slate-400 ">
+          <thead>
+            <tr className="border border-slate-400">
+              <th className="p-3 bg-slate-100">Daily Announcements</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dailyAnnouncements.map((ann: announcements) => {
+              return (
+                <tr key={ann.title}>
+                  <td className="border border-slate-200 p-3 flex flex-col gap-4">
+                    {ann.title}
+                    <div className="text-xs">{ann.date}</div>
+                    <div className="text-sm">{ann.announcement}</div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
