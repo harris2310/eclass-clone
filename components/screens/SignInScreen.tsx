e9789da94b63d40e5de42a817e3b5408f3a601c5
harris2310 <harris2310@hotmail.com>
2022-11-07 21:42:46 +0200

    Added google svg and changed layout

--- components/screens/SignInScreen.tsx
+++ components/screens/SignInScreen.tsx
@@ -3,6 +3,8 @@ import { signIn } from "next-auth/react";
 import { dailyAnnouncements } from "../../utils/constants";
 import Button from "../common/Button";
 import { Variant } from "../common/Button";
+import Image from "next/image";
+import googleSVG from "../../assets/google_icon.svg";
 
 type announcements = {
   title: string;
@@ -13,26 +15,13 @@ type announcements = {
 const SignInScreen = ({ providers }: any) => {
   return (
     <>
-      <h1 className="my-8 font-bold text-black">Welcome to E class</h1>
-      <div className="bg-gray-100 text-lg w-6/12 mx-auto my-5 border-orange-400 border-2 p-2">
-        The DUTHNET eClass platform is a complete Electronic Course Management
-        System. Access to the service is done using a simple web browser without
-        the need for external
-      </div>
-      {Object.values(providers).map((provider: any, i) => {
-        return (
-          <div key={i}>
-            <Button
-              variant={Variant.Google}
-              onClick={() => signIn(provider.id)}
-            >
-              Sign In With {provider.name}
-            </Button>
-          </div>
-        );
-      })}
-      <div className=" w-max mx-auto mt-16">
-        <table className="table-auto w-full border-separate p-2 border-spacing-y-4	border border-slate-400 ">
+      <div className="mx-auto mt-6">
+        <div className="bg-gray-100 text-lg w-6/12 mx-auto my-3 border-orange-400 border-2 p-2">
+          The DUTHNET eClass platform is a complete Electronic Course Management
+          System. Access to the service is done using a simple web browser
+          without the need for external
+        </div>
+        <table className="table-auto w-1/2 mx-auto border-separate p-2 border-spacing-y-4	border border-slate-400 ">
           <thead>
             <tr className="border border-slate-400">
               <th className="p-3 bg-slate-100">Daily Announcements</th>
@@ -52,6 +41,27 @@ const SignInScreen = ({ providers }: any) => {
             })}
           </tbody>
         </table>
+        {Object.values(providers).map((provider: any, i) => {
+          return (
+            <div key={i}>
+              <Button
+                variant={Variant.Google}
+                onClick={() => signIn(provider.id)}
+                className="flex mx-auto gap-2 mt-8"
+              >
+                <Image
+                  width="25"
+                  height="25"
+                  src={googleSVG}
+                  alt="google logo"
+                />{" "}
+                <div className="font-semibold">
+                  Sign In With {provider.name}
+                </div>
+              </Button>
+            </div>
+          );
+        })}
       </div>
     </>
   );
