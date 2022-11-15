b9e615a16c8cdc58013158d3680061ecba8aa9f1
harris2310 <harris2310@hotmail.com>
2022-11-15 19:04:35 +0200

    Added initial workflow for tests

---
+++ components/common/AnnouncementsTable.test.ts
@@ -0,0 +1,9 @@
+describe("describe outer", () => {
+  console.log("describe outer-a");
+
+  describe("describe inner 1", () => {
+    console.log("describe inner 1");
+
+    test("test 1", () => console.log("test 1"));
+  });
+});
