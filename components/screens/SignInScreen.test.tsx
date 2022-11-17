13a84cbd796f84aea7ef43be64d729463fea14d0
harris2310 <harris2310@hotmail.com>
2022-11-17 22:28:14 +0200

    Added self fullfilling test

---
+++ components/screens/SignInScreen.test.tsx
@@ -0,0 +1,34 @@
+import "@testing-library/jest-dom";
+import SignInScreen from "./SignInScreen";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import { ReactElement } from "react";
+
+const generalAnnouncements = [
+  {
+    id: 1,
+    date: "12-12-20010T",
+    title: "TIME TO TAKE A BATH",
+    content: "It`&apos`s ythat time of the day where you take a bath guys",
+  },
+];
+
+const providers = [
+  {
+    id: 1,
+    name: "Google",
+  },
+];
+
+describe("Sign in Screen test", () => {
+  test("Check if Providers exist", () => {
+    render(
+      <SignInScreen
+        providers={providers}
+        generalAnnouncements={generalAnnouncements}
+      />
+    );
+    expect(screen.getByText("Sign In With Google")).toHaveTextContent(
+      "Sign In With Google"
+    );
+  });
+});
