85e9484b9aa1573076b4be7aaa0373eadb53b2af
Harris Hanozidis <harris2310@hotmail.com>
2022-11-24 15:46:18 +0200

    [id] announcements type error

--- pages/courses/[id].tsx
+++ pages/courses/[id].tsx
@@ -5,7 +5,7 @@ import Head from "next/head";
 import prisma from "../../lib/prismadb";
 
 type Props = {
-  course: { id: number; term: number; description: string; name: string; open: boolean; annonuncements: Array<object> };
+  course: { id: number; term: number; description: string; name: string; open: boolean; annonuncements: Array<Object> };
 };
 
 const Course = ({ course }: Props) => {
